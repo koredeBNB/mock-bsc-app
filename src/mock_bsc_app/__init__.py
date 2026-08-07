c9d2eb86f3f1633e933406c6fd7bdfdb9e80241d
koredeBNB <korede.odubanjo@bnbchain.org>
2026-08-07 13:00:37 +0100

    Fix package exports for estimate_blob_fee.

--- src/mock_bsc_app/__init__.py
+++ src/mock_bsc_app/__init__.py
@@ -1,6 +1,12 @@
 from .block_sync import get_block_sync_status
-from .gas_fees import get_gas_fee_status
+from .gas_fees import estimate_blob_fee, get_gas_fee_status
 from .network_health import get_network_health
 from .validators import get_validator_status
 
-__all__ = ["get_block_sync_status", "get_gas_fee_status", "get_network_health", "get_validator_status"]
+__all__ = [
+    "get_block_sync_status",
+    "get_gas_fee_status",
+    "estimate_blob_fee",
+    "get_network_health",
+    "get_validator_status",
+]
